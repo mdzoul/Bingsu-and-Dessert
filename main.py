3da1ab53c5bcb8d1c3d167da5c8a10a23115408e
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-12-17 21:35:01 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, URLField
+from wtforms.validators import DataRequired
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe Name', validators=[DataRequired()])
+    location = URLField('Cafe Location on Google Maps (URL)', validators=[DataRequired()])
+    opening = StringField('Opening Time e.g. 8:00AM', validators=[DataRequired()])
+    closing = StringField('Closing Time e.g. 5:30PM', validators=[DataRequired()])
+    coffee = SelectField('Coffee Rating',
+                         choices=['☕️', '☕☕', '☕☕☕', '☕☕☕☕', '☕☕☕☕☕'],
+                         validators=[DataRequired()])
+    wifi = SelectField('Wifi Strength Rating',
+                       choices=['✘', '💪', '💪💪', '💪💪💪', '💪💪💪💪', '💪💪💪💪💪'],
+                       validators=[DataRequired()])
+    power = SelectField('Power Socket Availability',
+                        choices=['🔌', '🔌🔌', '🔌🔌🔌', '🔌🔌🔌🔌', '🔌🔌🔌🔌🔌'],
+                        validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        form_data = form.data
+        keys_to_remove = ['submit', 'csrf_token']
+        for k in keys_to_remove:
+            form_data.pop(k)
+        with open('cafe-data.csv', 'a') as csv_file:
+            csv_data = csv.writer(csv_file)
+            csv_data.writerow(form_data.values())
+        return redirect('/cafes')
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
