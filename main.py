3bdaf1534984fd90ce2f820ce79fa5b1f82411d7
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-12-21 02:56:16 +0800

    Hid SECRET_KEY

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, URLField
 from wtforms.validators import DataRequired
 import csv
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 Bootstrap(app)
 
 
